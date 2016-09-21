4d6089c3d27153bd767a70c4410886a45399179c
Eric Rosenberg <ehaydenr@gmail.com>
2016-09-21 15:55:49 -0500

    fix schemes path

--- backup/vars.sh
+++ backup/vars.sh
@@ -3,5 +3,5 @@
 SHARE="/mnt/external/share"
 BACKUP="/mnt/external/backup"
 BUCKET="s3://bdeep-dynamic/backup/net-share/"
-SCHEMES="/home/admin/Infrastructure/backup/schemes"
+SCHEMES="/usr/local/bdeep/Infrastructure/backup/schemes"
 EXCLUDE="/tmp/backup_exclude"
