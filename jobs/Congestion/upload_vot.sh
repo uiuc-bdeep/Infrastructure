2a2ee6af26669e776ba5655781ac73da58550678
Eric Rosenberg <ehaydenr@gmail.com>
2016-10-11 10:13:29 -0500

    add upload_vot

--- jobs/Congestion/upload_vot.sh
+++ jobs/Congestion/upload_vot.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "This is a dummy bash script"
+pushd /data/Congestion
+tar -czf /tmp/stream.tar.gz stream
+tar -czf /tmp/stores.tar.gz stores
+
+/usr/local/bin/aws s3 cp /tmp/stream.tar.gz s3://bdeep-concourse/VOT/
+/usr/local/bin/aws s3 cp /tmp/stores.tar.gz s3://bdeep-concourse/VOT/
+
+popd
